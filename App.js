9db47ac77f634c7c695dfb45081ba79bd784ac4a
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-10-23 07:32:22 +0530

    updating files

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, {useState} from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 // Import component
 import HomeScreen from "./App/screen/main/Home/HomeScreen";
 
+// Importing Fonts
+import * as Fonts from "expo-font";
+import AppLoading from "expo-app-loading";
+
+// Fetching Fonts
+const fetchFonts = () => {
+  return Fonts.loadAsync({
+    "open-sans": require("./App/assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./App/assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
+
+
 export default function App() {
+
+  // font state
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(error) => console.log(error)}
+      />
+    );
+  }
+
   return <HomeScreen />;
 }
 
