a47bcfb3a69cffce2ef12ed82d711debe51edce8
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-10-30 15:41:22 +0530

    Welcome Screen styled

--- App/screen/main/Auth/Welcome/Welcome.js
+++ App/screen/main/Auth/Welcome/Welcome.js
@@ -1,15 +1,34 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 // IMPORTING SvhXml for using svg
 import { SvgXml } from "react-native-svg";
 import WelcomeSVG from "../../../../assets/images/Auth/welcome";
 
+// Importing Colors
+import Colors from "../../../../constants/Colors";
+
+// Importing Components
+import CustomButton from "../../../../components/UI/CustomButton/CustomButton";
+import CustomButtonFull from "../../../../components/UI/CustomButtonFull/CustomButtonFull";
+
 const Welcome = () => {
   return (
     <View style={styles.container}>
+      <Text style={styles.welcomeMessage}>
+        Welcome to <Text style={styles.kheticulture}>Kheticulture</Text>
+      </Text>
+      <Text style={styles.explore}>Explore us</Text>
       <View style={styles.bookingImageContainer}>
         <SvgXml xml={WelcomeSVG} width="100%" height="400" />
+      </View>
+
+      <View style={styles.button}>
+        <CustomButton title="Login" />
+      </View>
+
+      <View style={styles.fullButton}>
+        <CustomButtonFull title="Signup" />
       </View>
     </View>
   );
@@ -24,5 +43,32 @@ const styles = StyleSheet.create({
 
   bookingImageContainer: {
     alignItems: "center",
+  },
+
+  welcomeMessage: {
+    textAlign: "center",
+    marginTop: 70,
+    fontSize: 20,
+    fontFamily: "open-sans",
+    color: Colors.grey,
+  },
+  kheticulture: {
+    fontSize: 20,
+    fontFamily: "open-sans-bold",
+    color: Colors.grey,
+  },
+  explore: {
+    textAlign: "center",
+    marginTop: 10,
+    fontSize: 16,
+    fontFamily: "open-sans",
+    color: Colors.grey,
+    marginBottom: 20,
+  },
+  button: {
+    marginTop: 50,
+  },
+  fullButton: {
+    marginTop: 20,
   },
 });
