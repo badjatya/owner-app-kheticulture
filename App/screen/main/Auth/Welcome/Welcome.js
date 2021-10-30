1ed6dbbddd03f5796fbaaa6bf41ac89893cc07f4
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-10-30 15:22:48 +0530

    Welcome Screen styled

--- App/screen/main/Auth/Welcome/Welcome.js
+++ App/screen/main/Auth/Welcome/Welcome.js
@@ -1,14 +1,28 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+// IMPORTING SvhXml for using svg
+import { SvgXml } from "react-native-svg";
+import WelcomeSVG from "../../../../assets/images/Auth/welcome";
+
 const Welcome = () => {
   return (
-    <View>
-      <Text>Welcome</Text>
+    <View style={styles.container}>
+      <View style={styles.bookingImageContainer}>
+        <SvgXml xml={WelcomeSVG} width="100%" height="400" />
+      </View>
     </View>
   );
 };
 
 export default Welcome;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+
+  bookingImageContainer: {
+    alignItems: "center",
+  },
+});
